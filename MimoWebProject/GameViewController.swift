d204726bd711d6492d4bfa5c7bb6b79f9b91dba7
shalaska <yiazamat@hotmail.com>
2018-01-09 15:02:13 +0100

    End of checking input chapter

--- MimoWebProject/GameViewController.swift
+++ MimoWebProject/GameViewController.swift
@@ -9,27 +9,15 @@
 import UIKit
 
 class GameViewController: UIViewController {
+  
+  @IBOutlet weak var inputField: UITextField!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+  @IBAction func guess(_ sender: Any) {
+    if let input = self.inputField.text, input != ""  {
+      print("Input: \(input)")
+    } else {
+      print("no input")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+  }
+  
 }
