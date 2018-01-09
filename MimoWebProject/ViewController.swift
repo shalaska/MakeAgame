9e4756ac9e20bfa81c8cc78be377403c677d044c
shalaska <yiazamat@hotmail.com>
2018-01-09 11:04:21 +0100

    Initial Commit

---
+++ MimoWebProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MimoWebProject
+//
+//  Created by Joseph on 09.01.18.
+//  Copyright © 2018 Joseph. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
