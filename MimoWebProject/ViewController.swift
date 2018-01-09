5cae7f34b6aca50bd71eb4215391d0a128c73bf0
shalaska <yiazamat@hotmail.com>
2018-01-09 11:34:58 +0100

    End of Build and App project 2

--- MimoWebProject/ViewController.swift
+++ MimoWebProject/ViewController.swift
@@ -10,16 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var randomNumberLabel: UILabel!
+  var number = 10
+  var index = 0
+  let colors = [UIColor.red, UIColor.green, UIColor.blue]
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    self.newRandomNumber(self)
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  
+  @IBAction func newRandomNumber(_ sender: Any) {
+    self.number = Int(arc4random_uniform(6))
+    self.randomNumberLabel.text = "\(number)"
+    self.index += 1
+    if self.index > 2 {
+      self.index = 0
+    }
+    self.view.backgroundColor = self.colors[self.index]
   }
-
-
+  
 }
 
