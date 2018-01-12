080290c43b7b98705db05d33625c0552fa20765f
shalaska <yiazamat@hotmail.com>
2018-01-12 16:12:32 +0100

    End of challenges

--- MimoWebProject/MenuViewController.swift
+++ MimoWebProject/MenuViewController.swift
@@ -1,5 +1,21 @@
 import UIKit
 
 class MenuViewController: UIViewController {
-
+  
+  var index = 0
+  let colors = [UIColor.cyan, UIColor.brown, UIColor.purple]
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(changeColor), userInfo: nil, repeats: true)
+  }
+  
+  @objc func changeColor() {
+    self.index += 1
+    if self.index > 2 {
+      self.index = 0
+    }
+    self.view.backgroundColor = self.colors[self.index]
+  }
+  
 }
